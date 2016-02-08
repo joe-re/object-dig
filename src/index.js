1539039a504009150a5192bd96c110607f751f4c
joe-re <joe.tialtngo@gmail.com>
2016-02-08 09:14:40 +0900

    to be executable callback.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 module.exports = (target, ...keys) => {
   let digged = target;
   for (const key of keys) {
-    digged = digged[key];
+    if (typeof key === 'function') {
+      digged = key(digged);
+    } else {
+      digged = digged[key];
+    }
     if (typeof digged === 'undefined') {
       return undefined;
     }

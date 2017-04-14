88ddb5c450284c552c124f2fe54aa65f928e64e8
Zane Wolfgang Pickett <sirwolfgang@users.noreply.github.com>
2017-04-14 11:22:30 -0700

    Fix issue with undefined target case

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 module.exports = (target, ...keys) => {
   let digged = target;
   for (const key of keys) {
+    if (typeof digged === 'undefined') {
+      return undefined;
+    }
     if (typeof key === 'function') {
       digged = key(digged);
     } else {
       digged = digged[key];
-    }
-    if (typeof digged === 'undefined') {
-      return undefined;
     }
   };
   return digged;
